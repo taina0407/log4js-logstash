5ba38d2731571affe9c3c5fcc8d7af9be5d7e5cf
Tai NA <taina0407@gmail.com>
2016-06-07 20:47:51 +0700

    Reformat source code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var net = require('net');
 
 // Function to identify type of an object. 
 // Source: https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/ 
-var toType = function(obj) {
+var toType = function (obj) {
   return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
 }
 
@@ -15,23 +15,23 @@ var toType = function(obj) {
  * @returns {{@timestamp: string, @fields: {category: (categoryName|*), level: (levelStr|*)}}}
  */
 function logstashLayout(logEvt, fields) {
-    var messageData = logEvt.data[0], 
-        log = {
-            '@timestamp': (new Date()).toISOString(),
-            '@fields': {
-                category: logEvt.categoryName,
-                level: logEvt.level.levelStr
-            },
-            '@message' : (toType(messageData) === "string") ? messageData : JSON.stringify(messageData)
-        }
-    
-    for (var key in fields) {
-        if (typeof fields[key] !== 'function') {
-            log['@fields'][key] = fields[key];
-        }
+  var messageData = logEvt.data[0],
+    log = {
+      '@timestamp': (new Date()).toISOString(),
+      '@fields': {
+        category: logEvt.categoryName,
+        level: logEvt.level.levelStr
+      },
+      '@message': (toType(messageData) === "string") ? messageData : JSON.stringify(messageData)
     }
 
-    return JSON.stringify(log) + '\n';
+  for (var key in fields) {
+    if (typeof fields[key] !== 'function') {
+      log['@fields'][key] = fields[key];
+    }
+  }
+
+  return JSON.stringify(log) + '\n';
 }
 
 /**
@@ -45,48 +45,48 @@ function logstashLayout(logEvt, fields) {
  * @returns {Function}
  */
 function logStashAppender(config, fields, layout) {
-    var time = process.hrtime(),
-        messages = [],
-        timeOutId = 0;
+  var time = process.hrtime(),
+    messages = [],
+    timeOutId = 0;
 
-    layout = layout || logstashLayout;
+  layout = layout || logstashLayout;
 
-    //Setup the connection to logstash
-    function pushToStash(config, msg) {
-        var client = net.connect({host: config.host, port: config.port}, function () {
-            client.write(msg);
-            client.end();
-        });
-        //Fail silently
-        client.on('error', function (evt) {
-            if (true === config.debug) {
-                console.log('An error happened in the logstash appender!', evt);
-            }
-        });
+  //Setup the connection to logstash
+  function pushToStash(config, msg) {
+    var client = net.connect({host: config.host, port: config.port}, function () {
+      client.write(msg);
+      client.end();
+    });
+    //Fail silently
+    client.on('error', function (evt) {
+      if (true === config.debug) {
+        console.log('An error happened in the logstash appender!', evt);
+      }
+    });
+  }
+
+  return function (logEvt) {
+    //do stuff with the logging event
+    var data = layout(logEvt, fields);
+
+    if (config.batch === true) {
+      messages.push(data);
+      clearTimeout(timeOutId);
+      if ((process.hrtime(time)[0] >= config.batchTimeout || messages.length > config.batchSize)) {
+        pushToStash(config, messages.join());
+        time = process.hrtime();
+        messages = [];
+      } else {
+        timeOutId = setTimeout(function () {
+          pushToStash(config, messages.join());
+          time = process.hrtime();
+          messages = [];
+        }, 1000);
+      }
+    } else {
+      pushToStash(config, data);
     }
-
-    return function (logEvt) {
-        //do stuff with the logging event
-        var data = layout(logEvt, fields);
-
-        if (config.batch === true) {
-            messages.push(data);
-            clearTimeout(timeOutId);
-            if ((process.hrtime(time)[0] >= config.batchTimeout || messages.length > config.batchSize)) {
-                pushToStash(config, messages.join());
-                time = process.hrtime();
-                messages = [];
-            } else {
-                timeOutId = setTimeout(function () {
-                    pushToStash(config, messages.join());
-                    time = process.hrtime();
-                    messages = [];
-                }, 1000);
-            }
-        } else {
-            pushToStash(config, data);
-        }
-    };
+  };
 }
 
 /**
@@ -96,29 +96,29 @@ function logStashAppender(config, fields, layout) {
  * @returns {Function}
  */
 function configure(config) {
-    var key,
-        layout = null,
-        fields = {},
-        options = {
-            port: (typeof config.port === "number") ? config.port : 5959,
-            host: (typeof config.host === "string") ? config.host : 'localhost',
-            debug: config.debug || false
-        };
+  var key,
+    layout = null,
+    fields = {},
+    options = {
+      port: (typeof config.port === "number") ? config.port : 5959,
+      host: (typeof config.host === "string") ? config.host : 'localhost',
+      debug: config.debug || false
+    };
 
-    if (config.batch) {
-        options.batch = true;
-        options.batchSize = config.batch.size;
-        options.batchTimeout = config.batch.timeout;
-    }
+  if (config.batch) {
+    options.batch = true;
+    options.batchSize = config.batch.size;
+    options.batchTimeout = config.batch.timeout;
+  }
 
-    if (config.fields && typeof config.fields === 'object') {
-        for (key in config.fields) {
-            if (typeof config.fields[key] !== 'function') {
-                fields[key] = config.fields[key];
-            }
-        }
+  if (config.fields && typeof config.fields === 'object') {
+    for (key in config.fields) {
+      if (typeof config.fields[key] !== 'function') {
+        fields[key] = config.fields[key];
+      }
     }
-    return logStashAppender(options, fields, layout);
+  }
+  return logStashAppender(options, fields, layout);
 }
 
 exports.appender = logStashAppender;
--- tests/test.js
+++ tests/test.js
@@ -1,73 +1,73 @@
 var net = require('net'),
-    log4jsAppender = require('../index');
+  log4jsAppender = require('../index');
 
 module.exports = {
-    setUp: function (callback) {
-        "use strict";
-        var self = this;
-        this.config = {
-            "category": "TEST",
-            "type": "log4js-logstash",
-            "host": "localhost",
-            "port": 5959,
-            "fields": {
-                "instance": "MyAwsInstance",
-                "source": "myApp",
-                "environment": "development"
-            }
-        };
+  setUp: function (callback) {
+    "use strict";
+    var self = this;
+    this.config = {
+      "category": "TEST",
+      "type": "log4js-logstash",
+      "host": "localhost",
+      "port": 5959,
+      "fields": {
+        "instance": "MyAwsInstance",
+        "source": "myApp",
+        "environment": "development"
+      }
+    };
 
-        this.testServer = net.createServer();
-        this.testServer.on('listening', function () {
-            callback();
-        });
-        this.testServer.listen(this.config.port, this.config.host);
-    },
-    tearDown: function (callback) {
-        "use strict";
-        var self = this;
-        this.testServer.on('close', function () {
-            callback();
-        });
-        this.testServer.close();
-    },
-    'Check basic usage': function (test) {
-        "use strict";
-        test.expect(2);
-        var self = this,
-            message = 'Im awesome',
-            log;
+    this.testServer = net.createServer();
+    this.testServer.on('listening', function () {
+      callback();
+    });
+    this.testServer.listen(this.config.port, this.config.host);
+  },
+  tearDown: function (callback) {
+    "use strict";
+    var self = this;
+    this.testServer.on('close', function () {
+      callback();
+    });
+    this.testServer.close();
+  },
+  'Check basic usage': function (test) {
+    "use strict";
+    test.expect(2);
+    var self = this,
+      message = 'Im awesome',
+      log;
 
-        test.doesNotThrow(function () {
-            log = log4jsAppender.configure(self.config);
-        }, 'Error', 'Configure shouldn\'t throw an error');
+    test.doesNotThrow(function () {
+      log = log4jsAppender.configure(self.config);
+    }, 'Error', 'Configure shouldn\'t throw an error');
 
-        this.testServer.on('connection', function (con) {
-            con.on('data', function (data) {
-                test.strictEqual(JSON.parse(data.toString())['@message'], message, 'data should be message!');
-                test.done();
-            });
-        });
-        log({ level: {levelStr: 'FOO'}, categoryName: 'BAR', data: [message] });
-    },
-    'When log message is an object, it gets converted to a string': function (test) {
-        "use strict";
-        var self = this,
-            message = { error: true, errorMessage: "something happened"},
-            log;
+    this.testServer.on('connection', function (con) {
+      con.on('data', function (data) {
+        test.strictEqual(JSON.parse(data.toString())['@message'], message, 'data should be message!');
+        test.done();
+      });
+    });
+    log({level: {levelStr: 'FOO'}, categoryName: 'BAR', data: [message]});
+  },
+  'When log message is an object, it gets converted to a string': function (test) {
+    "use strict";
+    var self = this,
+      message = {error: true, errorMessage: "something happened"},
+      log;
 
-        test.doesNotThrow(function () {
-            log = log4jsAppender.configure(self.config);
-        }, 'Error', 'Configure shouldn\'t throw an error');
+    test.doesNotThrow(function () {
+      log = log4jsAppender.configure(self.config);
+    }, 'Error', 'Configure shouldn\'t throw an error');
 
-        this.testServer.on('connection', function (con) {
-            con.on('data', function (data) {
-                test.strictEqual(JSON.parse(data.toString())['@message'],
-                    '{"error":true,"errorMessage":"something happened"}', 
-                    'data should be message!');
-                test.done();
-            });
-        });
-        log({ level: {levelStr: 'FOO'}, categoryName: 'BAR', data: [message] });
-    }
+    this.testServer.on('connection', function (con) {
+      con.on('data', function (data) {
+        test.strictEqual(JSON.parse(data.toString())['@message'],
+          '{"error":true,"errorMessage":"something happened"}',
+          'data should be message!');
+        test.done();
+      });
+    });
+    log({level: {levelStr: 'FOO'}, categoryName: 'BAR', data: [message]});
+  }
 };
